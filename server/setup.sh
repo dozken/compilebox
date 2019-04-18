d1677564272e2490268f7ed52114be640f017bd4
Ilya Chekalsky <ilya@chekalsky.com>
2019-04-18 21:42:21 +0200

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 apt update
-apt install -y git upstart upstart-sysv docker.io
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+apt install -y git upstart upstart-sysv
 
 cd /opt
 git clone https://github.com/chekalskiy/compilebox /opt/compilebox
